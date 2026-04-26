d5418384e4b81586ee28a272db46daf903bfb9eb
Sara Al-hachami <hi0172@wayne.edu>
2026-04-26 18:47:40 -0400

    remove node_modules, use DATABASE_URL

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'collegewtf',
-  password: 'Familjaime1!',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
