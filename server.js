ee61ea3256b0370fe8789ff5c3a06c1c9f5c0f36
Sara Al-hachami <hi0172@wayne.edu>
2026-04-26 19:06:08 -0400

    serve frontend from express

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname)));
+
 const storiesRoutes = require('./routes_stories_v2');
 app.use('/api/stories', storiesRoutes);
 
-app.listen(3001, () => {
-  console.log('Server running on http://localhost:3001');
+app.listen(process.env.PORT || 3001, () => {
+  console.log('Server running');
 });
